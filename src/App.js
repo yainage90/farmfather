2a16d8dcee5a60c0f80e51fecdf8a153ea59ddf7
yaincoding <yaincoding@gmail.com>
2021-07-16 01:20:15 +0900

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-function App() {
+import { Layout } from "antd";
+import "./App.css";
+
+import AppHeader from "./components/Header";
+import AppFooter from "./components/Footer";
+
+import Home from "./routes/Home";
+import Courses from "./routes/Courses";
+import Register from "./routes/Register";
+import LoginPopup from "./components/LoginPopup";
+
+const { Content } = Layout;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout
+        className="layout"
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <AppHeader />
+        <Content style={contentStyle}>
+          <Route path="/" component={Home} exact />
+          <Route path="/register" component={Register} exact />
+          <Route path="/courses" component={Courses} exact />
+        </Content>
+        <AppFooter />
+      </Layout>
+    </BrowserRouter>
   );
-}
+};
+
+const contentStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  background: "#fff",
+  padding: "40px",
+  width: "100%",
+  height: "100%",
+  marginTop: "80px",
+};
 
 export default App;
