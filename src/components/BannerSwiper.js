be0f7b59fe672ad95eb8ec4baab9f3e8e8575302
yaincoding <yaincoding@gmail.com>
2021-07-17 21:59:49 +0900

    replace 'img' tag to 'Image' component of antd

--- src/components/BannerSwiper.js
+++ src/components/BannerSwiper.js
@@ -5,6 +5,7 @@ import "swiper/swiper.scss";
 import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
 import appBanner from "../farmfather_banner.png";
+import { Image } from "antd";
 
 SwiperCore.use([Navigation, Pagination]);
 
@@ -24,10 +25,10 @@ const BannerSwiper = () => {
       }}
     >
       <SwiperSlide>
-        <img width="100%" height="100%" src={appBanner} />
+        <Image width="100%" height="100%" src={appBanner} />
       </SwiperSlide>
       <SwiperSlide>
-        <img
+        <Image
           width="100%"
           height="100%"
           src="https://cdn.inflearn.com/public/main_sliders/eead4cae-85b7-4faf-b8e4-6d7680037c2d/%5B%E1%84%86%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%92%E1%85%B5%E1%84%8B%E1%85%A5%E1%84%85%E1%85%A9%5D%E1%84%8C%E1%85%B5%E1%84%80%E1%85%B3%E1%86%B7%E1%84%92%E1%85%A1%E1%86%AF%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%8C%E1%85%AE%E1%86%BC_521.gif"
