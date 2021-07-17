907910c9f8bd21e62b64ed8457e1ec1a696a5a31
yaincoding <yaincoding@gmail.com>
2021-07-17 22:07:13 +0900

    removed rout for login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { Layout } from "antd";
@@ -9,12 +9,23 @@ import AppFooter from "./components/Footer";
 
 import Home from "./routes/Home";
 import Courses from "./routes/Courses";
+import Course from "./routes/Course";
 import Register from "./routes/Register";
 import LoginPopup from "./components/LoginPopup";
 
 const { Content } = Layout;
 
 const App = () => {
+  const [loginPopupDisplay, setLoginPopupDisplay] = useState("none");
+
+  const showLoginPopup = () => {
+    setLoginPopupDisplay("flex");
+  };
+
+  const closeLoginPopup = () => {
+    setLoginPopupDisplay("none");
+  };
+
   return (
     <BrowserRouter>
       <Layout
@@ -23,11 +34,16 @@ const App = () => {
           minHeight: "100vh",
         }}
       >
-        <AppHeader />
+        <LoginPopup
+          display={loginPopupDisplay}
+          closeLoginPopup={closeLoginPopup}
+        />
+        <AppHeader showLoginPopup={showLoginPopup} />
         <Content style={contentStyle}>
           <Route path="/" component={Home} exact />
           <Route path="/register" component={Register} exact />
           <Route path="/courses" component={Courses} exact />
+          <Route path="/course/:courseId" component={Course} exact />
         </Content>
         <AppFooter />
       </Layout>
