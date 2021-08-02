a1abdae003d227e82316f521d2e2092ae4710179
yaincoding <yaincoding@gmail.com>
2021-08-03 00:53:43 +0900

    proxy url changed

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
-    createProxyMiddleware("/api", {
-      target: "http://localhost:5000",
+    createProxyMiddleware("/", {
+      target: "http://localhost:8080",
       changeOrigin: true,
     })
   );
